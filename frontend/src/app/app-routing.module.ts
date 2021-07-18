7ca126b00eecdd5de20854cebf2d45979ffe0d07
Vilhelmus25 <vorvie@gmail.com>
2021-07-18 17:27:05 +0200

    sidenav base

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArchiveComponent } from './page/archive/archive.component';
+import { CertificateComponent } from './page/certificate/certificate.component';
+import { HomeComponent } from './page/home/home.component';
+import { StatisticsComponent } from './page/statistics/statistics.component';
+import { SubscribersComponent } from './page/subscribers/subscribers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'subscribers',
+    component: SubscribersComponent,
+  },
+  {
+    path: 'archive',
+    component: ArchiveComponent,
+  },
+  {
+    path: 'certificate',
+    component: CertificateComponent,
+  },
+  {
+    path: 'statistics',
+    component: StatisticsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,24 @@ import { HttpClientModule } from '@angular/common/http/';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { SideNavComponent } from './common/side-nav/side-nav.component';
+import { HomeComponent } from './page/home/home.component';
+import { SubscribersComponent } from './page/subscribers/subscribers.component';
+import { ArchiveComponent } from './page/archive/archive.component';
+import { StatisticsComponent } from './page/statistics/statistics.component';
+import { CertificateComponent } from './page/certificate/certificate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SideNavComponent,
+    HomeComponent,
+    SubscribersComponent,
+    ArchiveComponent,
+    StatisticsComponent,
+    CertificateComponent
   ],
   imports: [
     BrowserModule,
